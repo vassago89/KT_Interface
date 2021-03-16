7bbd4b174bc4833f21934ca2640a639ffe145364
vassago89 <vassago89@gmail.com>
2021-03-16 12:21:06 +0900

    Tutorial

---
+++ Tutorial/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Tutorial
+{
+    /// <summary>
+    /// App.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var main = new MainWindow();
+            main.ShowDialog();
+        }
+    }
+}
