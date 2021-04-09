d1f803f331928a961c05ce97b89566cf22886fb9
vassago89 <vassago89@gmail.com>
2021-04-09 11:18:19 +0900

    UsageService 추가

---
+++ KT_Interface.Core/Services/UsageService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+namespace KT_Interface.Core.Services
+{
+    public class UsageService
+    {
+        public static class PerformanceInfo
+        {
+            [DllImport("psapi.dll", SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            private static extern bool GetPerformanceInfo([Out] out PerformanceInformation PerformanceInformation, [In] int Size);
+
+            [StructLayout(LayoutKind.Sequential)]
+            public struct PerformanceInformation
+            {
+                public int Size;
+                public IntPtr CommitTotal;
+                public IntPtr CommitLimit;
+                public IntPtr CommitPeak;
+                public IntPtr PhysicalTotal;
+                public IntPtr PhysicalAvailable;
+                public IntPtr SystemCache;
+                public IntPtr KernelTotal;
+                public IntPtr KernelPaged;
+                public IntPtr KernelNonPaged;
+                public IntPtr PageSize;
+                public int HandlesCount;
+                public int ProcessCount;
+                public int ThreadCount;
+            }
+
+            public static Int64 GetTotalMemoryInMiB()
+            {
+                PerformanceInformation pi = new PerformanceInformation();
+                if (GetPerformanceInfo(out pi, Marshal.SizeOf(pi)))
+                {
+                    return Convert.ToInt64((pi.PhysicalTotal.ToInt64() * pi.PageSize.ToInt64() / 1048576));
+                }
+                else
+                {
+                    return -1;
+                }
+
+            }
+        }
+
+        public double CpuUsage 
+        {
+              get
+              {
+                  return _cpuUsage.NextValue();
+              }
+        }
+
+        public double MemoryAvailable
+        {
+            get
+            {
+               return _memoryAvailable.NextValue();
+            }
+        }
+
+        public double MemoryTotal { get; private set; }
+
+        public IEnumerable<DriveInfo> DriveInfos { get; private set; }
+
+        PerformanceCounter _cpuUsage;
+        PerformanceCounter _memoryAvailable;
+
+        public UsageService()
+        {
+            _cpuUsage = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            _memoryAvailable = new PerformanceCounter("Memory", "Available MBytes");
+            MemoryTotal = PerformanceInfo.GetTotalMemoryInMiB();
+            DriveInfos = DriveInfo.GetDrives().Where(d => d.DriveType == DriveType.Fixed);
+        }
+    }
+}
---
+++ KT_Interface/ViewModels/UsageViewModel.cs
@@ -0,0 +1,106 @@
+﻿using KT_Interface.Core.Services;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT_Interface.ViewModels
+{
+    class BindableDriveInfo : BindableBase
+    {
+        public string Name { get; private set; }
+        public double TotalSize { get; private set; }
+
+        private double _usageSize;
+        public double UsageSize
+        {
+            get 
+            {
+                return _usageSize;
+            }
+            set 
+            {
+                SetProperty(ref _usageSize, value);
+            }
+        }
+
+        public BindableDriveInfo(string name, double totalSize)
+        {
+            Name = name;
+            TotalSize = totalSize;
+        }
+    }
+
+    class UsageViewModel : BindableBase
+    {
+        public ObservableCollection<BindableDriveInfo> DriveInfos { get; private set; }
+
+        private double _cpuUsage;
+        public double CpuUsage
+        {
+            get 
+            {
+                return _cpuUsage;
+            }
+            set 
+            {
+                SetProperty(ref _cpuUsage, value);
+            }
+        }
+
+        private double _memoryUsage;
+        public double MemoryUsage
+        {
+            get 
+            {
+                return _memoryUsage;
+            }
+            set
+            {
+                SetProperty(ref _memoryUsage, value);
+            }
+        }
+
+        private double _memoryTotal;
+        public double MemoryTotal
+        {
+            get
+            {
+                return _memoryTotal;
+            }
+            set
+            {
+                SetProperty(ref _memoryTotal, value);
+            }
+        }
+
+        public UsageViewModel(UsageService usageService)
+        {
+            MemoryTotal = usageService.MemoryTotal / 1024;
+
+            DriveInfos = new ObservableCollection<BindableDriveInfo>();
+            foreach (var info in usageService.DriveInfos)
+                DriveInfos.Add(new BindableDriveInfo(info.Name, info.TotalSize / 1024.0 / 1024.0 / 1024.0));
+
+            Task.Factory.StartNew(async () =>
+            {
+                while (true)
+                {
+                    CpuUsage = usageService.CpuUsage;
+                    MemoryUsage = MemoryTotal - (usageService.MemoryAvailable / 1024);
+
+                    foreach (var info in usageService.DriveInfos)
+                    {
+                        var founded = DriveInfos.First(d => d.Name == info.Name);
+                        founded.UsageSize = founded.TotalSize - info.AvailableFreeSpace / 1024.0 / 1024.0 / 1024.0;
+                    }
+
+                    await Task.Delay(1000);
+                }
+            }, TaskCreationOptions.LongRunning);
+        }
+    }
+}
